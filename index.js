1cd4f2aa2a808b858a243355fedefde80b948e71
George  Pianka <gpianka1@gmail.com>
2019-04-18 08:11:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function driverNamesWithRevenueOver(drivers, revenue) {
 
 function exactMatch(drivers, match) {
   return drivers.filter(function (driver) {
-      return (driver.keys === match.keys)
+      return (Object.keys(driver) === Object.keys(match))
     });
  }
